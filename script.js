8f3b4385b90a8a20a7b79c9773656d5078f44d9a
Clare-Boniface <clarelgarfield@hotmail.com>
2023-08-29 14:21:30 +0100

    submit final project

--- script.js
+++ script.js
@@ -34,10 +34,17 @@ generateColor();
 //Event Listeners
 
 randomColorBtn.addEventListener("click", getRandomColor);
-// colorBtnEl.addEventListener("click", generateColor);
 colorSchemeContainerEl.addEventListener("click", copyHexCode);
 toggleBtnEl.addEventListener("click", toggle);
 
+//Functions
+
+colorBtnEl.addEventListener("click", function (e) {
+  e.preventDefault();
+  generateColor();
+});
+
+// Generate a random color and update the color scheme
 function getRandomColor(e) {
   e.preventDefault();
   let hexNumberArray = [];
@@ -50,19 +57,13 @@ function getRandomColor(e) {
   generateColor();
 }
 
-colorBtnEl.addEventListener("click", function (e) {
-  e.preventDefault();
-  generateColor();
-});
-
+// Fetch color scheme data from an API and update the display
 async function generateColor() {
   currentSeedColor = seedColorEl.value.substring(1);
   const response = await fetch(
     `https://www.thecolorapi.com/scheme?hex=${currentSeedColor}&mode=${mode.value}&count=5`
   );
   const data = await response.json();
-  console.log(data);
-
   const colorDivs = data.colors
     .map((color) => {
       const hexColor = color.hex.value;
@@ -75,8 +76,9 @@ async function generateColor() {
   colorSchemeContainerEl.innerHTML = colorDivs;
 }
 
-function copyHexCode(event) {
-  const clickedElement = event.target;
+// Copy the clicked hex code to clipboard and show snackbar notification
+function copyHexCode(e) {
+  const clickedElement = e.target;
   if (
     clickedElement.classList.contains("hex-code") ||
     clickedElement.classList.contains("color-column")
@@ -84,9 +86,7 @@ function copyHexCode(event) {
     const hexCodeElement = clickedElement.classList.contains("hex-code")
       ? clickedElement
       : clickedElement.querySelector(".hex-code");
-
     const hexCode = hexCodeElement.textContent.trim();
-
     copyToClipboard(hexCode).then(() => {
       displayCopied(hexCodeElement, hexCode);
       showSnackbar();
@@ -94,27 +94,30 @@ function copyHexCode(event) {
   }
 }
 
+// Copy text to the clipboard
 function copyToClipboard(text) {
   return navigator.clipboard.writeText(text);
 }
 
+// Display "Copied!" status and reset after a delay
 function displayCopied(element, hexCode) {
   element.textContent = "Copied!";
   element.classList.add("click-hex-code");
-
   setTimeout(() => {
     element.textContent = hexCode;
     element.classList.remove("click-hex-code");
   }, 2000);
 }
 
-function toggle() {
-  document.body.classList.toggle("dark-mode");
-}
-
+// Show snackbar notification and remove after a delay
 function showSnackbar() {
   snackbar.classList.add("show");
   setTimeout(function () {
     snackbar.classList.remove("show");
   }, 2000);
+}
+
+// Toggle between dark and light modes
+function toggle() {
+  document.body.classList.toggle("dark-mode");
 }
